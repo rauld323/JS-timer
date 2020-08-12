1a77d0df893c8c20f9eec115f8b2d4e5be2e1fee
Raul Davila <rauldavila323@gmail.com>
2020-08-11 21:57:00 -0700

    start, tick, pause all work and callbacks were added.

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
-class Timer{
-    //First define the constructor method.
-    constructor(durationInput, startButton, pauseButton){   
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-
-        //Event listerner has been binded to the elements
-        this.startButton.addEventListener('click', this.start);
-        //This Stops the ticking
-        this.pauseButton.addEventListener('click', this.pause);
-    }
-
-    start = () => {
-        //Activates 'tick' as soon as start is clicked
-        this.tick();
-        // This will make sure that tick runs every second. intervalr also returns a reference number
-        this.interval = setInterval(this.tick, 1000);
-    }
-
-    pause = () => {
-        clearInterval(this.interval);
-    }
-
-    tick = () => {
-        console.log("tick")
-    }    
-}
-
+//Query Selectors
 const durartionInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause')
 
-const timer = new Timer(durartionInput, startButton, pauseButton)
+
+const timer = new Timer(durartionInput, startButton, pauseButton, {
+    onStart(){
+        console.log('Timer Started')
+    },
+    onTick(){
+        console.log('Timer just ticked down')
+    },
+    onComplete(){
+        console.log('Timer is completed')
+    }
+})
 
