850c8358c98b70d0f53358e4cf1729801f024ada
Raul Davila <rauldavila323@gmail.com>
2020-08-13 22:59:25 -0700

    Timer is functional.

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const circle = document.querySelector('circle')
 const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter); 
 
-let currentOffset = 0 ;
+let durationl
 const timer = new Timer(durartionInput, startButton, pauseButton, {
-    onStart(){
-        console.log('Timer Started')
+    onStart(totalDuration){
+        duration = totalDuration
     },
-    onTick(){
-        circle.setAttribute('stroke-dashoffset', currentOffset)
-        currentOffset = currentOffset - 1;
+    onTick(timeRemaining){
+        circle.setAttribute('stroke-dashoffset',
+            perimeter * timeRemaining / duration - perimeter
+        )
     },
     onComplete(){
         console.log('Timer is completed')
